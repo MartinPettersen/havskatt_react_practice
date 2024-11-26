a8e83106a162afbe4d4f7387bc17cf5d111469e6
MartinPettersen <searchingtheskyfor@gmail.com>
2024-11-26 11:21:22 +0100

    add contact page

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import Parallax from "./components/(utils)/Parallax";
+import image from "./assets/yoann-boyer-VZSIm_cNAL8-unsplash.jpg";
+import map from "./assets/bestmap.png";
 
 const Contant = () => {
-  return (
-    <div>Contant</div>
-  )
-}
 
-export default Contant
+  const [subject, setSubject] = useState("")
+  const [content, setContent] = useState("")
+  const [email, setEmail] = useState("")
+
+  return (
+    <Parallax imageUrl={image}>
+      <div className="flex item-end justify-center">
+        <div className="flex items-center  justify-center  w-[70%] ">
+          <div className="flex flex-col space-y-4"> 
+
+          <p>Telefon: 999 999 99</p>
+          <p>E-post: HavSkatt@gmail.com</p>
+          <p>Adresse: Fiskebryggen 12, 0150 Oslo.</p>
+          <img src={map} />
+          <div>
+            <p>Emne:</p>
+            <input className="w-full" placeholder="Emne"
+            value = {subject}
+            onChange={e => setSubject(e.target.value)}
+            />
+          </div>
+          <div>
+            <p>(Innhold:)</p>
+            <textarea className="w-full h-20" placeholder="Innhold"
+            value = {content}
+            onChange={e => setContent(e.target.value)}/>
+          </div>
+          <div>
+            <p>E-post:</p>
+            <input className="w-full " placeholder="Din epost"
+            value = {email}
+            onChange={e => setEmail(e.target.value)}/>
+          </div>
+          <div className="bg-[#4A90E2] p-4 flex items-center justify-center">
+            <p className="font-bold text-lg">Send</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </Parallax>
+  );
+};
+
+export default Contant;
