a172b39366a6240f0149d579a87d238357066b78
MartinPettersen <searchingtheskyfor@gmail.com>
2024-11-27 14:05:47 +0100

    add hamburger menu for mobile

--- src/components/(navigation)/Navbar.tsx
+++ src/components/(navigation)/Navbar.tsx
@@ -1,30 +1,43 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid'
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <>
       <div className="flex flex-col items-center z-50 fixed w-full">
-        <h1 className="text-[#FF6F61] m-4 text-4xl md:text-8xl">Havets Skatter</h1>
-        <div className="bg-gradient-to-r from-transparent via-coralRed to-transparent  flex items-center justify-center  w-full md:w-[70%]">
+        <h1 className="text-[#FF6F61] m-4 text-4xl md:text-8xl">
+          Havets Skatter
+        </h1>
+        <div
+          className={`bg-gradient-to-r from-transparent via-coralRed to-transparent  flex items-center justify-center  w-full md:w-[70%] ${showMenu ? "" : "hidden"} md:flex`}
+        >
           <nav className="flex flex-col md:flex-row p-4 justify-center items-center md:justify-evenly md:w-[76%]">
-
-          <Link to="/" className="text-[#1c1c1c]">
-            Hjem
-          </Link>
-          <Link to="/menu" className="text-[#1c1c1c]">
-            Meny
-          </Link>
-          <Link to="/ordertable" className="text-[#1c1c1c]">
-            Bestill
-          </Link>
-          <Link to="/about" className="text-[#1c1c1c]">
-            Om Oss
-          </Link>
-          <Link to="/contact" className="text-[#1c1c1c]">
-            Kontakt Oss
-          </Link>
+            <Link to="/" className="text-[#1c1c1c]">
+              Hjem
+            </Link>
+            <Link to="/menu" className="text-[#1c1c1c]">
+              Meny
+            </Link>
+            <Link to="/ordertable" className="text-[#1c1c1c]">
+              Bestill
+            </Link>
+            <Link to="/about" className="text-[#1c1c1c]">
+              Om Oss
+            </Link>
+            <Link to="/contact" className="text-[#1c1c1c]">
+              Kontakt Oss
+            </Link>
+            <button className="md:hidden text-[#1c1c1c]" onClick={() => setShowMenu(!showMenu)}>
+            <XMarkIcon className="size-4 text-[#1c1c1c]" />
+            </button>
           </nav>
         </div>
+        <button onClick={() => setShowMenu(!showMenu)} className={`bg-gradient-to-r from-transparent via-coralRed to-transparent p-2 w-full flex items-center justify-center md:hidden ${showMenu ? "hidden" : ""}`}>
+        <Bars3Icon className="size-6  text-[#1c1c1c]" />
+        </button>
       </div>
     </>
   );
