7eecf253b37a659fb806b5592de5e57b6b719be6
MartinPettersen <searchingtheskyfor@gmail.com>
2024-11-28 11:41:52 +0100

    imrpve semantics in menu

--- src/Views/Menu.tsx
+++ src/Views/Menu.tsx
@@ -8,16 +8,22 @@ const Menu = () => {
   return (
     <div className="flex flex-col space-y-8 items-center w-full py-48  justify-center flex-grow ">
       {menu["menues"].map((course, i) => (
-        <div key={i} className="w-[80%] md:w-[100%]">
+        <section
+          key={i}
+          className="w-[80%] md:w-[100%]"
+          aria-labelledby={course.name}
+        >
           <div className="w-full flex items-center justify-center">
-          <h2 className="font-bold text-2xl p-4">{course.name}</h2>
-            </div>
-          <div className="space-y-0">
-            {course["retter"].map((dish, i) => (
-              <Dish  key={i} dish={dish} />
-            ))}
+            <h2 id={course.name} className="font-bold text-2xl p-4">{course.name}</h2>
           </div>
-        </div>
+          <ul className="space-y-0">
+            {course["retter"].map((dish, i) => (
+              <li key={i}>
+                <Dish dish={dish} />
+              </li>
+            ))}
+          </ul>
+        </section>
       ))}
     </div>
   );
